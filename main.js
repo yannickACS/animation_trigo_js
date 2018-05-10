b6179dd4b830a0048ef48c7971a1db110b6a0bd6
yannickACS <yannick.k@codeur.online>
2018-05-10 18:51:09 +0200

    Playing with lissajous curves

--- main.js
+++ main.js
@@ -15,25 +15,30 @@ window.addEventListener('DOMContentLoaded', function(event){
 	let snake_angleX = 0,
 		snake_angleY = 0,
 		snake_radiusX = 200,
-		snake_radiusY = 200,
+		snake_radiusY = 40,
 		snake_offsetHeight = screenHeight * 0.3,
 		snake_offsetWidth = screenWidth * 0.3,
-		snake_speed1 = 0.04,
-		snake_speed2 = 0.06,
-		snake_totalBall = 8,
-		snake_slice = (Math.PI * 2) /8;
+		snake_speed1 = 0.03,
+		snake_speed2 = 0.04,
+		snake_totalBall = 64,
+		snake_slice = (Math.PI * 2) / snake_totalBall,
+		snake_baseAlpha = 0.55,
+		snake_offsetAlpha = 0.45;
 
 	render();
 
 	function render(){
-
 		context.clearRect(0, 0, screenWidth, screenHeight);
 		for (let boule = 0; boule < snake_totalBall; boule ++){
 			let x = centerX + Math.cos(snake_angleX + snake_slice * boule) * snake_radiusX,
-				y = centerY + Math.sin(snake_angleY + snake_slice * boule) * snake_radiusY;
+				y = centerY - Math.sin(snake_angleY + snake_slice * boule) * snake_radiusY;
+			// let alpha = snake_baseAlpha + Math.cos(snake_angleY + snake_slice * boule) * snake_offsetAlpha;
+			
 
 			context.beginPath();
 			context.arc(x, y, 20, 0, Math.PI * 2, false);
+			// context.fillStyle = "rgba(0, 0, 0, " + alpha + ")";
+			context.fillStyle = 'rgb(' + Math.floor(255 - (255 / snake_totalBall) * boule) + ',' + Math.floor(255 - (255 / snake_totalBall) * boule) + ',0)';
 			context.fill();
 		}
 		snake_angleX += snake_speed1;
